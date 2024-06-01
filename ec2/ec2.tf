5b146ed06d4feae3114b7081c5c45efb4e4a4c38
adityamurali155 <adityamurali2@gmail.com>
2024-06-01 20:22:03 +0530

    changes

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -17,7 +17,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  vpc_sec
   tags = {
     Name = "HelloWorld"
   }
