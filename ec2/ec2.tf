c6218212ce8f013d090466900f39674875cb1acb
adityamurali155 <adityamurali2@gmail.com>
2024-06-04 16:04:45 +0530

    added changes

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -17,14 +17,10 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  subnet_id = var.subnet_id
   tags = {
     Name = "HelloWorld"
   }
 }
-variable "vpc_id" {
-  type = string
-}
 variable "subnet_id" {
-  type = string
+  default = true
 }
