c589416c5ee6316c8885ab4263dd32f876472f5a
adityamurali155 <adityamurali2@gmail.com>
2024-06-17 16:30:07 +0530

    modifed vpc

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,18 +1,9 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "main_subnet"
-  }
+data "aws_subnet" "default" {
+  vloe = data.aws_vpc.default.id[0]
 }
 # output "vm_subnet_id" {
 #   value = aws_subnet.main.id
