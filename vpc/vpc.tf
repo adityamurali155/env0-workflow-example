4c646b8d3bc4b1ee48006b6ece5c42185c9a4cb2
adityamurali155 <adityamurali2@gmail.com>
2024-06-17 16:59:06 +0530

    modifed vpc

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,14 +6,6 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "main_subnet"
-  }
-}
 # output "vm_subnet_id" {
 #   value = aws_subnet.main.id
 # }
